bcec8f5f1622f4f78fbc0dfe3b4d66d01935edf6
Joshua Beach <Joshua Beach@JBEACH-01>
2019-09-22 15:13:27 -0400

    parser

---
+++ Parser/src/LexicalAnalyzer.java
@@ -0,0 +1,340 @@
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class LexicalAnalyzer {
+
+	/*
+	 * Wilson Joshua Beach, N01109437 Lexical Analyzer Project 1 Compilers class
+	 * Program to be executed from command line expecting file as arg
+	 */
+	public LexicalAnalyzer(File tester) {
+		try {
+			boolean commentMode = false;
+			char currentToken;
+			String line = "";
+			String tokenBuilder = "";
+			Scanner scanner = new Scanner(tester);
+			PrintWriter writer = new PrintWriter("tokens.txt");
+
+			// iterates file setting each line equal to a string
+			while (scanner.hasNextLine()) {
+				line = scanner.nextLine();
+				// skip all this if the line is empty
+				if (!line.isEmpty()) {
+					//System.out.println("INPUT:" + line);
+					for (int i = 0; i < line.length(); i++) {
+						currentToken = line.charAt(i);
+						// switch checks if the token is a spec char
+						switch (currentToken) {
+
+						case '+':
+
+							if (commentMode == false) {
+								//System.out.println(line.charAt(i));
+								writer.println(line.charAt(i));
+							}
+
+							break;
+						case '-':
+
+							if (commentMode == false) {
+								// System.out.println(line.charAt(i));
+								writer.println(line.charAt(i));
+							}
+
+							break;
+						case '/':
+							// checks for * following / to turn out comment mode on
+							if (commentMode == false) {
+								if (i + 1 < line.length()) {
+									if (line.charAt(i + 1) == '*') {
+										commentMode = true;
+										i++;
+									} else {
+										if (line.charAt(i + 1) == '/') {
+											while (i + 1 < line.length()) {
+												i++;
+											}
+
+										} else
+											// System.out.println(line.charAt(i));
+											writer.println(line.charAt(i));
+
+									}
+								} else
+									// System.out.println(line.charAt(i));
+									writer.println(line.charAt(i));
+							}
+
+							break;
+						case '*':
+							// checks for any backslash following directly after the * to turn comments off
+							if (commentMode == true) {
+								if (i + 1 < line.length()) {
+									if (line.charAt(i + 1) == '/') {
+										commentMode = false;
+										i++;
+									}
+
+								}
+
+							} else {
+								writer.println(line.charAt(i));
+								// System.out.println(line.charAt(i));
+							}
+
+							break;
+						case '<':
+							// accepts/checks for any <=
+							if (commentMode == false) {
+								if (i + 1 < line.length()) {
+									if (line.charAt(i + 1) == '=') {
+										// System.out.println("<=");
+										writer.println("<=");
+										i++;
+									} else
+										// System.out.println(line.charAt(i));
+										writer.println(line.charAt(i));
+
+								} else
+									// System.out.println(line.charAt(i));
+									writer.println(line.charAt(i));
+							} else {
+								// System.out.println(line.charAt(i));
+								writer.println(line.charAt(i));
+							}
+
+							break;
+						case '>':
+							// accepts/check for any >=
+							if (commentMode == false) {
+								if (i + 1 < line.length()) {
+									if (line.charAt(i + 1) == '=') {
+										// System.out.println(">=");
+										writer.println(">=");
+										i++;
+									} else
+										// System.out.println(line.charAt(i));
+										writer.println(line.charAt(i));
+
+								} else
+									// System.out.println(line.charAt(i));
+									writer.println(line.charAt(i));
+							}
+
+							break;
+
+						case '=':
+							// accept ='s and check for =='s
+							if (commentMode == false) {
+								if (i + 1 < line.length()) {
+									if (line.charAt(i + 1) == '=') {
+										// System.out.println("==");
+										writer.println("==");
+										i++;
+									} else
+										// System.out.println(line.charAt(i));
+										writer.println(line.charAt(i));
+
+								} else
+									// System.out.println(line.charAt(i));
+									writer.println(line.charAt(i));
+							}
+
+							break;
+						case '!':
+							// includes check to give error for only ! but accept !=
+							if (commentMode == false) {
+								if (i + 1 < line.length()) {
+									if (line.charAt(i + 1) == '=') {
+										// System.out.println("!=");
+										writer.println("!=");
+										i++;
+									} else
+										// System.out.println("Error: " + line.charAt(i));
+										System.out.println("Error Invalid Character found in LexicalAnalyzer");
+									System.exit(0);
+									writer.println("Error: " + line.charAt(i));
+
+								} else
+									System.out.println("Error: " + line.charAt(i));
+								System.out.println("Error Invalid Character found in LexicalAnalyzer");
+								System.exit(0);
+								writer.println("Error: " + line.charAt(i));
+							}
+
+							break;
+						// checks if is any of these are the current token
+						case ',':
+
+							if (commentMode == false) {
+								//System.out.println(line.charAt(i));
+								writer.println(line.charAt(i));
+							}
+
+							break;
+						case '(':
+
+							if (commentMode == false) {
+								writer.println(line.charAt(i));
+								//System.out.println(line.charAt(i));
+							}
+
+							break;
+						case ')':
+
+							if (commentMode == false) {
+								writer.println(line.charAt(i));
+								//System.out.println(line.charAt(i));
+							}
+
+							break;
+						case '[':
+
+							if (commentMode == false) {
+								writer.println(line.charAt(i));
+								//System.out.println(line.charAt(i));
+							}
+
+							break;
+						case ']':
+
+							if (commentMode == false) {
+								writer.println(line.charAt(i));
+								//System.out.println(line.charAt(i));
+							}
+
+							break;
+						case '{':
+
+							if (commentMode == false) {
+								writer.println(line.charAt(i));
+								//System.out.println(line.charAt(i));
+							}
+
+							break;
+						case '}':
+
+							if (commentMode == false) {
+								writer.println(line.charAt(i));
+								//System.out.println(line.charAt(i));
+							}
+
+							break;
+						case ';':
+
+							if (commentMode == false) {
+								writer.println(line.charAt(i));
+								//System.out.println(line.charAt(i));
+							}
+
+							break;
+						// check for whitespace
+						case ' ':
+
+							break;
+						// if our token is not a spec char, it can only be 1 of 4 things:
+						// a ID, Keyword, error, or num
+						default:
+
+							if (commentMode == false) {
+								// if we are a potiential ID or num or Keyword
+								if (Character.isAlphabetic(line.charAt(i)) || Character.isDigit(line.charAt(i))) {
+									if (Character.isAlphabetic(line.charAt(i))) {
+										while (i < line.length() && Character.isAlphabetic(line.charAt(i))) {
+											tokenBuilder = tokenBuilder + line.charAt(i);
+											i++;
+										}
+										// check if its a num
+									} else if (Character.isDigit(line.charAt(i))) {
+										while (i < line.length() && Character.isDigit(line.charAt(i))) {
+											tokenBuilder = tokenBuilder + line.charAt(i);
+											i++;
+										}
+									}
+
+								} // at this point we are formed a continous token comprised only of digits or
+									// Nums
+									// so here I am checking if our token matches a keyword
+								if (isKeyword(tokenBuilder)) {
+									writer.println("K: "+tokenBuilder);
+									//System.out.println("Keyword: " + tokenBuilder);
+									tokenBuilder = "";
+									i--;
+									// verify to check it is only digits, then print as a num
+								} else if (isOnlyDigits(tokenBuilder)) {
+									writer.println("NUM: "+tokenBuilder);
+									//System.out.println("NUM: " + tokenBuilder);
+									tokenBuilder = "";
+									i--;
+									// verify it is only alphabetical chars and NOT a keyword
+								} else if (isOnlyAlphas(tokenBuilder)) {
+									writer.println("ID: "+tokenBuilder);
+									//System.out.println("ID: " + tokenBuilder);
+									tokenBuilder = "";
+									i--;
+
+								} else {
+									// if its not alphas, spec chars, or nums or whitespace, then error
+									writer.println("Error: "+line.charAt(i));
+									System.out.println("Error Invalid Character found in LexicalAnalyzer");
+									System.exit(0);
+									//System.out.println("Error:" + line.charAt(i));
+
+								}
+							}
+							break;
+						}
+					}
+					// formatting
+					//System.out.println();
+				}
+			}
+			writer.println("#");
+			scanner.close();
+			writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+		}
+
+	} 
+
+//private helper functions for verifying tokens
+	private static boolean isKeyword(String x) {
+		boolean y = false;
+		String[] keywords = { "while", "int", "void", "return", "if", "else"};
+		for (int j = 0; j < keywords.length; j++) {
+			if (x.equals(keywords[j])) {
+				y = true;
+			}
+		}
+		return y;
+	}
+
+	private static boolean isOnlyDigits(String x) {
+		boolean y = false;
+
+		for (int j = 0; j < x.length(); j++) {
+			if (Character.isDigit(x.charAt(j))) {
+				y = true;
+			}
+		}
+		return y;
+	}
+
+	private static boolean isOnlyAlphas(String x) {
+		boolean y = false;
+
+		for (int j = 0; j < x.length(); j++) {
+			if (Character.isAlphabetic(x.codePointAt(j))) {
+				y = true;
+			}
+		}
+		return y;
+	}
+
+}
---
+++ Parser/src/Parser.java
@@ -0,0 +1,454 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Parser {
+	static String token = null;
+	static Scanner scanner = null;
+
+	public static void main(String[] args) {
+
+		File tester = new File(args[0]);
+		LexicalAnalyzer LexLuther = new LexicalAnalyzer(tester);
+		tester = new File("tokens.txt");
+
+		try {
+			scanner = new Scanner(tester);
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found in parser.");
+
+		}
+		token = scanner.nextLine();
+		program();
+
+		if (token.equals("#")) {
+			System.out.println("Accept");
+		} else
+			reject();
+
+	} // end of main
+
+////////////////////C- language methods begin here///////////////////////////////////////////////
+
+	public static void program() {
+		declarationList();
+	}
+
+	public static void declarationList() {
+		declaration();
+		declarationList1();
+	}
+
+	public static void declarationList1() {
+		if (token.contains("K: ")) {
+			declaration();
+			declarationList1();
+		} 
+	}
+
+	public static void declaration() {
+		typeSpecifier();
+		if (token.contains("ID: ")) {
+			returnToken();
+			declaration1();
+
+		} else reject();  //ghjhgfd
+	}
+
+	public static void declaration1() {
+		if (token.equals(";") || token.equals("[")) {
+			varDeclaration1();
+		} else if (token.equals("(")) {
+			funDeclaration1();
+		} else
+			reject();
+	}
+
+	public static void varDeclaration() {
+		
+		typeSpecifier();
+		if (token.contains("ID: ")) {
+			returnToken();
+			varDeclaration1();
+		} else
+			reject();
+		
+	}
+
+	public static void varDeclaration1() {
+		if (token.equals(";")) {
+			returnToken();
+		} else if (token.equals("[")) {
+			returnToken();
+			if (token.contains("NUM: ")) {
+				returnToken();
+				if (token.equals("]")) {
+					returnToken();
+					if (token.equals(";")) {
+						returnToken();
+					} else
+						reject();
+				} else
+					reject();
+			} else
+				reject();
+		} else
+			reject();
+	}
+
+	public static void typeSpecifier() {
+		if (token.equals("K: int") || token.equals("K: void")) {
+			returnToken();
+		} else
+			reject();
+	}
+
+	public static void funDeclaration1() {
+		if (token.equals("(")) {
+			returnToken();
+			params();
+			if (token.equals(")")) {
+				returnToken();
+				compoundStmt();
+				
+			} else
+				reject();
+		}
+
+	}
+
+	public static void params() {
+		if (token.equals("K: void")) {
+			returnToken();
+		} else
+			paramsList();
+
+	}
+
+	public static void paramsList() {
+		param();
+		paramsList1();
+	}
+
+	public static void paramsList1() {
+		if (token.equals(",")) {
+			returnToken();
+			param();
+			paramsList1();
+		} else
+			epsilon();
+
+	}
+
+	public static void param() {
+		typeSpecifier();
+		if (token.contains("ID: ")) {
+			returnToken();
+			if (token.equals("[")) {
+				returnToken();
+				if (token.equals("]")) {
+					returnToken();
+				}
+
+			}
+		}
+	}
+
+	public static void compoundStmt() {
+		if (token.equals("{")) {
+			returnToken();
+			localDeclaration();
+			statementList();
+			if (token.equals("}")) {
+				returnToken();
+			}
+		}
+	}
+
+	public static void localDeclaration() {
+		
+		if (token.equals("K: int") || token.equals("K: float") || token.equals("K: void")) {
+			varDeclaration();
+			localDeclaration();
+			
+		}
+
+	}
+
+	public static void statementList() {
+	
+		if (token.contains("K: ") || token.contains("ID: ") || token.equals("(") || token.equals("{")
+				|| token.equals(";")) {
+			statement();
+			statementList();
+		}
+	}
+
+	public static void statement() {
+		if (token.equals("K: if")) {
+			selectionStmt();
+		} else if (token.equals("K: return")) {
+			returnStmt();
+		} else if (token.equals("K: while")) {
+			iterationStmt();
+		} else if (token.equals("{")) {
+			compoundStmt();
+		} else
+			expressionStmt();
+
+	}
+
+	public static void expressionStmt() {
+		
+		if (token.equals(";")) {
+			returnToken();
+		} else {
+			expression();
+			if (token.equals(";")) {
+				returnToken();
+			} else
+				reject();
+		}
+	}
+
+	public static void selectionStmt() {
+
+		if (token.equals("K: if")) {
+			returnToken();
+			if (token.equals("(")) {
+				returnToken();
+				expression();
+				if (token.equals(")")) {
+					returnToken();
+					statement();
+					if (token.equals("K: else")) {
+						returnToken();
+						statement();
+					}
+				} else
+					reject();
+			} else
+				reject();
+		} else
+			reject();
+	}
+
+	public static void iterationStmt() {
+		if (token.equals("K: while")) {
+			returnToken();
+			if (token.equals("(")) {
+				returnToken();
+				expression();
+				if (token.equals(")")) {
+					returnToken();
+					statement();
+				} else
+					reject();
+			} else
+				reject();
+		} else
+			reject();
+	}
+
+	public static void returnStmt() {
+		if (token.equals("K: return")) {
+			returnToken();
+			if (token.equals(";")) {
+				returnToken();
+			} else {
+				expression();
+				if (token.equals(";")) {
+					returnToken();
+				} else
+					reject();
+			}
+		} else
+			reject();
+	}
+
+	public static void expression() {
+		additiveExpression();
+		if (token.equals("<=") || token.equals("<") || token.equals(">") || token.equals(">=") || token.equals("==")
+				|| token.equals("!=")) {
+			relop();
+			additiveExpression();
+		}
+	}
+
+	public static void var1() {
+		if (token.equals("[")) {
+			returnToken();
+			expression();
+			if (token.equals("]")) {
+				returnToken();
+				if (token.equals("=")) {
+					returnToken();
+					if (token.contains("ID: ")) {
+						returnToken();
+						if (token.equals("[")) {
+							returnToken();
+							expression();
+							if (token.equals("]")) {
+								returnToken();
+							} else
+								reject();
+						} else
+							reject();
+
+					} else
+						expression();
+
+				} else
+					epsilon();
+
+			} else
+				reject();
+
+		} else if (token.equals("=")) {
+			returnToken();
+			expression();
+		} 
+
+	}
+
+	public static void relop() {
+		if (token.equals("<=") || token.equals("<") || token.equals(">") || token.equals(">=") || token.equals("==")
+				|| token.equals("!=")) {
+			returnToken();
+		} else
+			reject();
+
+	}
+
+	public static void additiveExpression() {
+		term();
+		additiveExpression1();
+	}
+
+	public static void additiveExpression1() {
+		
+		if (token.equals("+") || token.equals("-")) {
+			addop();
+			term();
+			additiveExpression1();
+		}
+	}
+
+	public static void addop() {
+		if (token.equals("+")) {
+			returnToken();
+		} else if (token.equals("-")) {
+			returnToken();
+		} else
+			reject();
+	}
+
+	public static void term() {
+		factor();
+		term1();
+	}
+
+	public static void term1() {
+		if (token.equals("*") || token.equals("/")) {
+			mulop();
+			factor();
+			term1();
+		}
+	}
+
+	public static void mulop() {
+		if (token.equals("*")) {
+			returnToken();
+		} else if (token.equals("/")) {
+			returnToken();
+		} else
+			reject();
+	}
+
+	public static void factor() {
+		if (token.contains("NUM: ")) {
+			returnToken();
+		} else if (token.contains("ID: ")) {
+			returnToken();
+			factor1();
+		} else if (token.equals("(")) {
+			returnToken();
+			expression();
+			if (token.equals(")")) {
+				returnToken();
+			} else
+				reject();
+
+		}
+
+	}
+
+	public static void factor1() {
+		if (token.equals("(")) {
+			call1();
+		} else {
+			var1();
+		}
+
+	}
+
+	public static void call() {
+		if (token.contains("ID: ")) {
+			returnToken();
+			call1();
+		} else
+			reject();
+	}
+
+	public static void call1() {
+
+		if (token.equals("(")) {
+			returnToken();
+			args();
+			if (token.equals(")")) {
+				returnToken();
+			} else
+				reject();
+		} else
+			reject();
+	}
+
+	public static void args() {
+
+		if (!token.equals(")")) {
+			argList();
+		}
+	}
+
+	public static void argList() {
+		expression();
+		argList1();
+	}
+
+	public static void argList1() {
+		if (token.equals(",")) {
+			returnToken();
+			expression();
+			argList1();
+		}
+	}
+
+	public static void reject() {
+		System.out.println("Reject on Token: " + token);
+		System.exit(0);
+	}
+
+	public static void returnToken() {
+		System.out.println(token);
+		token = scanner.nextLine();
+	}
+
+	public static void epsilon() {
+		// nothing
+	}
+
+	public static void test() {
+		System.out.println("@@@@@@@@@@     " + token + "           @@@@@@@@@");
+	}
+}// end of entire code (parser class)
